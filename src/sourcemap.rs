0d8a99f344b9803f36b828113a18b71238d284df
Tazmondo <fondcoolman@outlook.com>
2023-10-31 03:16:02 +0000

    add sourcemap require resolution

--- src/sourcemap.rs
+++ src/sourcemap.rs
@@ -35,3 +35,57 @@ impl SourcemapNode {
 pub fn parse_sourcemap(sourcemap: &str) -> Result<SourcemapNode, Error> {
     from_str(&sourcemap)
 }
+
+pub fn node_search_name<'a>(
+    root: &'a SourcemapNode,
+    search_name: &str,
+) -> Option<&'a SourcemapNode> {
+    root.children.iter().find(|child| child.name == search_name)
+}
+
+// more awful code
+// TODO: Make this prettier and easier to follow
+pub fn resolve_require<'a>(
+    node_path: &Vec<&'a SourcemapNode>,
+    require: &Vec<String>,
+) -> Option<&'a SourcemapNode> {
+    let Some(require_head) = require.first() else {
+        return None;
+    };
+
+    let mut require_tail = &require[..];
+    let mut node_path_index = node_path.len() - 1;
+
+    let mut current_node = match require_head.as_str() {
+        "script" => {
+            require_tail = &require_tail[1..];
+
+            node_path_index -= 1;
+            node_path[node_path_index + 1]
+        }
+        _ => node_path.first().unwrap(),
+    };
+
+    for node_string in require_tail {
+        match node_string.as_str() {
+            "Parent" => {
+                current_node = node_path[node_path_index];
+
+                if node_path_index == 0 {
+                    break;
+                }
+                node_path_index -= 1;
+            }
+            child_string => {
+                current_node = match node_search_name(current_node, child_string) {
+                    Some(node) => node,
+                    None => {
+                        return None;
+                    }
+                }
+            }
+        }
+    }
+
+    Some(current_node)
+}
