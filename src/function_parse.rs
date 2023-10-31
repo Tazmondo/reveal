99e94dbc2dc15e39baa7c549020ebffa1d9217ed
Tazmondo <fondcoolman@outlook.com>
2023-10-31 00:52:49 +0000

    add require arguments parser

--- src/function_parse.rs
+++ src/function_parse.rs
@@ -1,4 +1,77 @@
-use full_moon::ast::{FunctionCall, Prefix};
+use full_moon::ast::{
+    Call, Expression, FunctionArgs, FunctionCall, Index, Prefix, Suffix, Value, Var,
+};
+
+fn expression_to_string(expression: &Expression) -> Option<Vec<String>> {
+    match expression {
+        Expression::Parentheses {
+            contained: _,
+            expression,
+        } => expression_to_string(expression),
+        Expression::Value {
+            value,
+            type_assertion: _,
+        } => match &**value {
+            //  Get reference to box contents
+            Value::Var(var) => match var {
+                Var::Name(name) => Some(vec![name.to_string()]),
+                Var::Expression(expression) => {
+                    let prefix = match expression.prefix() {
+                        Prefix::Name(name) => Some(name.to_string()),
+                        _ => None,
+                    };
+
+                    let suffixes: Vec<Option<String>> = expression
+                        .suffixes()
+                        .map(|suffix| match suffix {
+                            Suffix::Index(index) => match index {
+                                Index::Dot { dot: _, name } => Some(name.to_string()),
+                                _ => None,
+                            },
+                            _ => None,
+                        })
+                        .collect();
+
+                    vec![prefix]
+                        .into_iter()
+                        .chain(suffixes.into_iter())
+                        .collect()
+                }
+                _ => None,
+            },
+            _ => None,
+        },
+        _ => None,
+    }
+}
+
+fn get_require_tokens(func: &FunctionCall) -> Option<Vec<String>> {
+    let mut suffixes = func.suffixes();
+
+    let Some(suffix) = suffixes.next() else {
+        return None;
+    };
+
+    let args: Option<Vec<String>> = match suffix {
+        Suffix::Call(call) => match call {
+            Call::AnonymousCall(args) => match args {
+                FunctionArgs::Parentheses {
+                    parentheses: _,
+                    arguments,
+                } => match arguments.iter().next() {
+                    // Only take the first argument since it's require
+                    None => None,
+                    Some(expression) => expression_to_string(expression),
+                },
+                _ => None,
+            },
+            _ => None,
+        },
+        _ => None,
+    };
+
+    args
+}
 
 fn remove_preceding_comments(str: &String) -> Option<String> {
     str.lines()
@@ -20,5 +93,10 @@ pub fn print_function(file_path: &str, func: &FunctionCall) {
 
     let is_require = prefix == "require";
 
-    println!("{}: {} {}", file_path, prefix, is_require)
+    if is_require {
+        let Some(require_path) = get_require_tokens(func) else {
+            return;
+        };
+        println!("{}: {:?} ", file_path, require_path);
+    }
 }
