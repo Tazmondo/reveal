0affe3b550d687c79781f898c5b0159314cb5e5b
Tazmondo <fondcoolman@outlook.com>
2023-11-08 18:28:34 +0000

    basic graph display

--- reveal-js/main.js
+++ reveal-js/main.js
@@ -17,33 +17,47 @@ const svg = d3.select("#my_dataviz")
 
 const dataUrl = "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_network.json"
 d3.json("./testdata").then(function (data) {
+  var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-  // Initialize the links
-  const link = svg
+  var simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function (d) { return d.id; }).distance(40))
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(width / 2, height / 2));
+
+  var link = svg.append("g")
+    .attr("class", "links")
     .selectAll("line")
     .data(data.links)
-    .join("line")
-    .style("stroke", "#aaa")
+    .enter().append("line")
+    .attr("stroke-width", function (d) { return Math.sqrt(d.value); })
+    .attr("stroke", function (d) { return "#ffffff" });
 
-  // Initialize the nodes
-  const node = svg
-    .selectAll("circle")
+
+  var node = svg.selectAll(".node")
     .data(data.nodes)
-    .join("circle")
-    .attr("r", 20)
-    .style("fill", "#69b3a2")
+    .enter().append("g")
+    .attr("class", "node")
+    .call(d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended));
 
-  // Let's list the force we wanna apply on the network
-  const simulation = d3.forceSimulation(data.nodes)                 // Force algorithm is applied to data.nodes
-    .force("link", d3.forceLink()                               // This force provides links between nodes
-      .id(function (d) { return d.id; })                     // This provide  the id of a node
-      .links(data.links)                                    // and this the list of links
-    )
-    .force("charge", d3.forceManyBody().strength(-400))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
-    .force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
-    .on("end", ticked);
+  node.append("circle")
+    .attr("r", 5)
+    .attr("fill", function (d) { return color(d.group); });
 
-  // This function is run at each iteration of the force algorithm, updating the nodes position.
+  node.append("text")
+    .attr("dx", 6)
+    .text(function (d) { return d.id; })
+    .attr("fill", function (d) { return "#ffffff" })
+
+  simulation
+    .nodes(data.nodes)
+    .on("tick", ticked);
+
+  simulation.force("link")
+    .links(data.links);
+
   function ticked() {
     link
       .attr("x1", function (d) { return d.source.x; })
@@ -51,9 +65,30 @@ d3.json("./testdata").then(function (data) {
       .attr("x2", function (d) { return d.target.x; })
       .attr("y2", function (d) { return d.target.y; });
 
-    node
-      .attr("cx", function (d) { return d.x + 6; })
-      .attr("cy", function (d) { return d.y - 6; });
+    node.attr("transform", function (d) {
+      return "translate(" + d.x + "," + d.y + ")";
+    });
+  }
+
+  // Reheat the simulation when drag starts, and fix the subject position.
+  function dragstarted(event) {
+    if (!event.active) simulation.alphaTarget(0.3).restart();
+    event.subject.fx = event.subject.x;
+    event.subject.fy = event.subject.y;
+  }
+
+  // Update the subject (dragged node) position during drag.
+  function dragged(event) {
+    event.subject.fx = event.x;
+    event.subject.fy = event.y;
+  }
+
+  // Restore the target alpha so the simulation cools after dragging ends.
+  // Unfix the subject position now that it’s no longer being dragged.
+  function dragended(event) {
+    if (!event.active) simulation.alphaTarget(0);
+    event.subject.fx = null;
+    event.subject.fy = null;
   }
 
 });
